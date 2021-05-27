0a9ce209846183f222e973a3483266196d35bf72
HuseyinAA <hossain.ghazal@outlook.com>
2021-05-27 13:57:33 +0100

    initial commit!

---
+++ PlingPlangPlong/PlingPlangPlong/Raindrop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlingPlangPlong
+{
+    class Raindrop
+    {
+        public void Raindrops()
+        {
+            //Check if value not null
+            //Check 
+            string ppp = string.Empty; //PlingPlangPlong
+
+        }
+    }
+}
